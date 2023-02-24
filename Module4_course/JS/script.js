676dbaa2ee558e3f506eb34066ef1c9fee0f4407
Kim Cai <evalai@Kim-CaitekiMacBook-Pro.local>
2023-02-24 22:35:05 +0800

    Added assignment4

---
+++ Module4_course/JS/script.js
@@ -0,0 +1,40 @@
+// var company = new Object();
+// company.name = "facebook";
+// company.ceo = new Object();
+// company.ceo.firstName = "Mark"
+// company.ceo.favColor = "blue";
+// console.log(company);
+// console.log("Company CEO name is: " + company.ceo.firstName);
+// console.log (company["name"]);
+
+
+// var facebook = {
+//     name: "Facebook",
+//     ceo: {
+//         firstName: "Mark",
+//         favColor: "Blue"
+//     },
+//     $stock:110
+// };
+
+// console.log(facebook);
+
+// function multiply (x,y) {
+//     return x*y
+// }
+
+// console.log(multiply(5,4));
+
+// var a = 7;
+// var b = a;
+
+// console.log(a);
+// console.log(b);
+
+var a = {x:7};
+var b = a;
+
+b.x = 5;
+
+console.log(a);
+console.log(b);
